bc4059a9c7dd028d7ac336fbdb1b58570a0e2570
Diego Rodriguez <20186669@itla.edu.do>
2023-07-10 11:52:47 -0400

    Agregar archivos de proyecto.

---
+++ WAPS08/Models/ViewModels/AutoViewModels.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WAPS08.Models.ViewModels
+{
+	public class AutoTableViewModel
+	{
+		public int _Idauto { get; set; }
+		public string _Marca { get; set; }
+		public string _Modelo { get; set; }
+		public string _Anio { get; set; }
+		public string _Imagen { get; set; }
+		public string _Direccion { get; set; }
+
+		public string _TipoVehiculo { get; set; }
+
+		public string _Color { get; set; }
+
+		public string _Precio { get; set; }
+	}
+
+	public class AddAutoViewModels
+	{
+		[Required]
+		[Display(Name = "Marca")]
+		public string Marca { get; set; }
+
+		[Required]
+		[Display(Name = "Modelo Del Vehiculo")]
+		public string Modelo { get; set; }
+
+		[Required]
+		[Display(Name = "Ano Del Vehiculo")]
+		public string Anio { get; set; }
+
+		[Required]
+		[Display(Name = "Ruta de la Imagen")]
+		public string Imagen { get; set; }
+
+		[Required]
+		[Display(Name = "Direccion")]
+		public string Direccion { get; set; }
+		[Required]
+		[Display(Name = "Tipo de vehiculo")]
+		public string TipoVehiculo { get; set; }
+		[Required]
+		[Display(Name = "Color")]
+		public string Color { get; set; }
+		[Required]
+		[Display(Name = "Precio")]
+		public string Precio { get; set; }
+	}
+
+	public class EditAutoViewModels
+	{
+		public int IdAuto { get; set; }
+
+		[Required]
+		[Display(Name = "Marca")]
+		public string Marca { get; set; }
+
+		[Required]
+		[Display(Name = "Modelo Del Vehiculo")]
+		public string Modelo { get; set; }
+
+		[Required]
+		[Display(Name = "Ano Del Vehiculo")]
+		public string Anio { get; set; }
+
+		[Required]
+		[Display(Name = "Ruta de la Imagen")]
+		public string Imagen { get; set; }
+
+		[Required]
+		[Display(Name = "Direccion")]
+		public string Direccion { get; set; }
+		[Required]
+		[Display(Name = "Tipo de vehiculo")]
+		public string TipoVehiculo { get; set; }
+		[Required]
+		[Display(Name = "Color")]
+		public string Color { get; set; }
+		[Required]
+		[Display(Name = "Precio")]
+		public string Precio { get; set; }
+	}
+}
